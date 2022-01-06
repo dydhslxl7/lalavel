42947d38f2f4abc44392c8053e0bd293c806e153
dydhslxl7 <dydhslxl7@naver.com>
2022-01-06 21:57:38 +0900

    엘로퀀트ORM_Articles_foreign_issue

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'content'];
+
+    // public function user() {
+    //     return $this->belongsTo(User::class);
+    // }
+
+    public function tags() {
+        return $this->belongsToMany(Tag::class);
+    }
 }
